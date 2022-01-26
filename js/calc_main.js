4a46d45829f90972aa8f04711ca041ba8d98bedd
lazer3356 <98434434+lazer3356@users.noreply.github.com>
2022-01-26 23:18:02 +0500

    Update calc_main.js

--- js/calc_main.js
+++ js/calc_main.js
@@ -132,10 +132,9 @@ $(document).ready(function(){
 				$("#clientPrice, #print_clientPrice").text("$ " + numberWithCommas(clientPrice));
 				$("#serial_num").text("S # 0125" + round_zero_decimal_digits(profit) + "00");
 				
-				//$("#profit, #print_profit").text("$ " + profit);
+				let currentDate = new Date().toLocaleString();
 				
-				
-				
+				$("#date").text(currentDate);	
 				
 				$("#resultsTable").show();
 				
@@ -144,61 +143,6 @@ $(document).ready(function(){
 				}, 1000);
 				
 				$('#editor').append(print_input_data);
-				
-				
-				/*
-				$("#print_list_price_a").text("$ " + list_price_a);
-				$("#print_list_price_b").text("$ " + list_price_b);
-				$("#print_list_price_c").text("$ " + list_price_c);
-				$("#print_vendor").text( $('#main_unit option:selected').text() );
-				$("#print_discount").text( discount * 100 + " % " );
-				*/
-				/*
-					
-				let list_price_a = Number(document.getElementById("list_price_a").value);
-				let list_price_b = Number(document.getElementById("list_price_b").value);
-				let list_price_c = Number(document.getElementById("list_price_c").value);
-				priceInput_Array = [list_price_a , list_price_b , list_price_c];
-				
-				let vendor = Number( $('#main_unit').val() );
-				let upCharge = Number( $('#main_unit option:selected').attr('data-upcharge') )/100;
-				console.log(upCharge);
-				let discount =  $('#discount').val() ? Number( $('#discount').val() )/100 : 0;
-				
-				let price = 0;
-				let totalCost = 0;
-				let clientPrice = 0;
-				
-				for( let i=0; i < priceInput_Array.length ; i++){
-						if( priceInput_Array[i] !=0 && priceInput_Array[i] != undefined && priceInput_Array[i] != null && priceInput_Array[i] > 0 )
-						{
-							price = round_2_digits ( Number(priceInput_Array[i]) * Number(vendor) );
-							totalCost += round_2_digits(price);
-							clientPrice += round_2_digits( price + (price * upCharge) - (price * discount) );
-						}
-				}
-				
-				let profit = round_2_digits(clientPrice - totalCost);
-				
-				$("#totalCost, #print_totalCost").text("$ " + totalCost);
-				$("#clientPrice, #print_clientPrice").text("$ " + clientPrice);
-				$("#profit, #print_profit").text("$ " + profit);
-				
-				
-				
-				$("#resultsTable").show();
-				
-				$('html, body').animate({
-					scrollTop: $("#resultsTable").offset().top
-				}, 1000);
-				
-				$("#print_list_price_a").text("$ " + list_price_a);
-				$("#print_list_price_b").text("$ " + list_price_b);
-				$("#print_list_price_c").text("$ " + list_price_c);
-				$("#print_vendor").text( $('#main_unit option:selected').text() );
-				$("#print_discount").text( discount * 100 + " % " );
-				
-				*/
 				
 			});	
 			
