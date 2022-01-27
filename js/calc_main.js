3f5abc2731ca34f4b022cc3964d5a535291066c8
lazer3356 <98434434+lazer3356@users.noreply.github.com>
2022-01-28 01:47:32 +0500

    final 1 with changes

--- js/calc_main.js
+++ js/calc_main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 			var Alphabet_Array = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 			let newfield = '';
 			var priceFieldHtml = '<div class="row input_field">' + 
-					'<div class="col-md-6">' + 
+					'<div class="col-md-5">' + 
 						'<label>' + 
 							'Group B' + 
 						'</label>' + 
@@ -20,7 +20,7 @@ $(document).ready(function(){
 							'<input type="text" id="" class="number_req form-control description" name ="" value=""/>' + 
 						'</div>' + 
 					'</div>' + 
-					'<div class="col-md-5">' + 
+					'<div class="col-md-3">' + 
 						'<label>' + 
 							'Price' + 
 						'</label>' + 
@@ -35,15 +35,15 @@ $(document).ready(function(){
 				function priceField(num1){
 					
 					return '<div class="row input_field">' + 
-					'<div class="col-md-6">' + 
+					'<div class="col-md-5">' + 
 						'<label>' + 
 							'Group ' + Alphabet_Array[num1] + 
 						'</label>' + 
 						'<div class="input_field_n_dollar">' + 
-							'<input type="text" id="" class="number_req form-control description" name ="" value=""/>' + 
+							'<input type="text" id="" class="number_req form-control description" name ="" value="Cabinets"/>' + 
 						'</div>' + 
-					'</div>' + 
-					'<div class="col-md-5">' + 
+					'</div>' +									
+					'<div class="col-md-3">' + 
 						'<label>' + 
 							'Price' + 
 						'</label>' + 
@@ -52,6 +52,15 @@ $(document).ready(function(){
 							'<input type="text" id="" class="number_req form-control list_price" name ="" value=""/>' + 
 						'</div>' + 	
 					'</div>' + 
+					'<div class="col-md-3 modification">' +
+						'<label>' +
+							'Modification' +
+						'</label>' +
+						'<div class="input_field_n_dollar">' +
+							'<span class="dollar_sign"> $ </span>' +
+							'<input type="text" id="list_price_m1" class="number_req form-control list_price_m" name ="list_price_m1" value=""/>' +
+						'</div>	' +
+					'</div>' +	
 					'<div class="col-md-1"> <label>&nbsp;</label> <button type="button" class="minus"> - </button></div>' + 
 				'</div>';
 					
@@ -73,6 +82,21 @@ $(document).ready(function(){
 			  }
 			});
 			
+			jQuery('.modification').hide();
+			
+			jQuery('#main_unit').change(function() {
+				var eventTypeName = $("#main_unit option:selected");
+
+				if (eventTypeName.is('[name="CNG"]') ) {
+					//$('#band_type_choices option[name="acoustic"]').text('Wedding Ceremony');
+					jQuery('.modification').show();
+				}else{
+					jQuery('.modification').hide();
+				}
+
+			});
+			
+			
 			var num1 = 0;
 			
 			jQuery( ".plus" ).click(function( event ){
@@ -81,6 +105,10 @@ $(document).ready(function(){
 					num1 = num1 + 1;
 					newfield = priceField(num1);
 					$(this).parent().parent().parent().append(newfield);
+				}
+				
+				if ( $("#main_unit option:selected").is('[name="CNG"]') ) {
+					jQuery('.modification').show();
 				}
 			});
 
@@ -93,7 +121,7 @@ $(document).ready(function(){
 
 				event.preventDefault();
 				
-				jQuery("#input_data_table_1, #input_data_table_2, #input_data_table_3, .table_print_1").remove();
+				jQuery("#input_data_table_1, #input_data_table_2, #input_data_table_3, #input_data_table_4, #input_data_table_5, .table_print_1, .br_line").remove();
 				
 				var validator = $( "#priceCalcForm" ).validate();
 					if( ! validator.form() ){
@@ -103,71 +131,120 @@ $(document).ready(function(){
 						return;
 
 					} 
-					
-					
-				
+
 				let price = 0;
 				let totalCost = 0;
 				let clientPrice = 0;
+				let listprice = 0;
+				let modificationprice = 0;
 				
 				var priceInput_Array = [];
 				
-				let vendor = Number( $('#main_unit').val() );
-				let upCharge = Number( $('#main_unit option:selected').attr('data-upcharge') )/100;
+				let name = jQuery('#main_name').val();
+				let projectName = jQuery('#project_name').val();
 				
-				let vendorSurcharge = $('#main_unit option:selected').attr('data-upcharge') ? Number( $('#main_unit option:selected').attr('data-surcharge') )/100 : 0;
+				let vendor = Number( jQuery('#main_unit').val() );
+				let upCharge = Number( jQuery('#main_unit option:selected').attr('data-upcharge') )/100;
 				
-				let discount =  $('#discount').val() ? Number( $('#discount').val() )/100 : 0;
+				let vendorSurcharge = jQuery('#main_unit option:selected').attr('data-upcharge') ? Number( $('#main_unit option:selected').attr('data-surcharge') )/100 : 0;
 				
-				let shipping =  $('#shipping').val() ? Number( $('#shipping').val() ) : 0;
-				let surcharge =  $('#surcharge').val() ? Number( $('#surcharge').val() ) : 0;
+				let discount =  jQuery('#discount').val() ? Number( $('#discount').val() )/100 : 0;
 				
-				let print_input_data = '<table id="input_data_table_3"> <tr> <th class="thead-dark"> Description</th> <th class="thead-dark"> </th></tr>';
+				let shipping =  jQuery('#shipping').val() ? Number( $('#shipping').val() ) : 0;
+				let surcharge =  jQuery('#surcharge').val() ? Number( $('#surcharge').val() ) : 0;
+				
+				let shipping_part =  Number( shipping ) / jQuery('.list_price').length ;
+				
+				
+				let print_project_info = '<table id="input_data_table_2" class="table_print_pdf"> ' + 
+										'<tr><td class=""> Name : ' + name +  ' </td></tr>' + 
+										'<tr><td class=""> Project Name : ' + projectName +  ' </td></tr>' +  
+										'</table><div class="br_line"><br/></div>';
+				
+				let print_cost_table = '<table id="input_data_table_3" class="table_print_pdf"> ' + 
+											'<tr>' + 
+												'<td> </td>' +
+												'<td>Retail</td>' +
+												'<td>List</td>' +
+												'<td>Cost</td>' +
+											'</tr>';
+											
+				let print_input_data = '<table id="input_data_table_4" class="table_print_pdf">';
 				
 				let print_ind_data = '';
 				
+				let print_cost_table_sub = '';
+				
+				let cnt1 = 0;
+				let modificationtotalPrice = 0;
 				
 				$('.list_price').each(function () {
 					
+					modificationprice = Number ( jQuery('.list_price_m ').eq(cnt1).val() );
+					modificationtotalPrice += modificationprice;
+					
 					price = Number( $(this).val() );
+					listprice = listprice + price;
 					price = price + ( Number( $(this).val() ) * Number(vendorSurcharge) );
 					//price = round_2_digits ( Number( $(this).val() ) * Number(vendor)  );
-					price = round_2_digits( price * Number(vendor) ) ;
+					price = round_2_digits( ( price * Number(vendor) ) ) ;
 					
 					totalCost += round_2_digits(price);
 					clientPrice += round_2_digits( price + (price * upCharge) - (price * discount) );
 					
-					print_input_data += '<tr><td>' + $(this).parent().parent().parent().find('.description').val() + ' </td>';
-					print_input_data += '<td> List Price $ ' + numberWithCommas( Number( $(this).val() ) ) + ' </td></tr>';
+					//print_input_data += '<tr><td>' + $(this).parent().parent().parent().find('.description').val() + ' </td>';
+					//print_input_data += '<td> List Price $ ' + numberWithCommas( Number( $(this).val() ) ) + ' </td></tr>';
 					
 					print_ind_data += '<tr><td>' + $(this).parent().parent().parent().find('.description').val() + ' </td>';
+					//print_ind_data += '<td>' + numberWithCommas( Number( clientPrice ) ) + ' </td>';
 					print_ind_data += '<td> $ ' + numberWithCommas( Number(price) ) + ' </td></tr>';
+					
+					
+					print_cost_table_sub +=  '<tr>' + 
+												'<td>' + $(this).parent().parent().parent().find('.description').val() + ' </td>' +
+												'<td>' + numberWithCommas( round_2_digits( price + (price * upCharge) - (price * discount) + shipping_part + modificationprice ) ) + ' </td>' +
+												'<td>' + numberWithCommas( Number( $(this).val() ) ) + ' </td>' +
+												'<td>' + numberWithCommas( round_2_digits(price + shipping_part + modificationprice) ) + ' </td>' +
+											'</tr>';
+					
+					cnt1++;					
 					
 				});
 				
 				
 
-				print_input_data += '<tr><td> Vendor </td>';
-				print_input_data += '<td>' + $('#main_unit option:selected').text() + ' </td></tr>';
+				print_input_data += '<tr>' + 
+										'<td> Vendor : ' + $('#main_unit option:selected').text() + ' </td>' + 
+										'<td> Client Level : ' + $('#discount option:selected').text() + ' </td>' + 
+									'</tr>';
+									
+				print_input_data += '<tr>' + 
+										'<td> Surcharges : ' + round_2_digits ( Number ( vendorSurcharge ) * 100 ) + ' </td>' + 
+										'<td> Shipping : ' + numberWithCommas(shipping) + ' </td>' + 
+									'</tr>';				
 				
-				print_input_data += '<tr><td> Other Charges </td>';
-				print_input_data += '<td>' +  round_2_digits ( Number ( vendorSurcharge ) * 100 ) + ' % </td></tr>';
+				print_input_data += '</table><div class="br_line"><br/></div>';
 				
-				print_input_data += '<tr><td> Shipping </td>';
-				print_input_data += '<td> $ ' + numberWithCommas(shipping) + ' </td></tr>';				
-				
-				print_input_data += '<tr><td> Discount </td>';
-				print_input_data += '<td>' + discount * 100 + ' % </td></tr>';
+				totalCost = totalCost + Number(shipping) + Number ( modificationtotalPrice ); // + Number(surcharge);
+				clientPrice = clientPrice + Number(shipping) + Number ( modificationtotalPrice ); // + Number(surcharge);
 				
 				
-				print_input_data += '</table>';
-				
-				totalCost = totalCost + Number(shipping); // + Number(surcharge);
-				clientPrice = clientPrice + Number(shipping); // + Number(surcharge);
+				print_cost_table += '<tr>' + 
+										'<td class="text-bold"> Total </td>' +
+										'<td class="text-bold"> $ ' + numberWithCommas( round_2_digits( clientPrice ) ) + ' </td>' +
+										'<td class="text-bold"> $ ' + numberWithCommas( round_2_digits( listprice ) ) + ' </td>' +
+										'<td class="text-bold"> $' +  numberWithCommas( round_2_digits(totalCost) ) + ' </td>' +
+									'</tr>';
+									
+				print_cost_table += print_cost_table_sub;
+				print_cost_table += '</table><div class="br_line"><br/></div>';
+											
+											
+											
 				
 				let profit = round_2_digits(clientPrice - totalCost);
 				
-				$("#totalCost").text("Total Cost Price is : $" + numberWithCommas(totalCost) );
+				//$("#totalCost").text("Total Cost Price is : $" + numberWithCommas(totalCost) );
 				$("#clientPrice").text("$ " + numberWithCommas(clientPrice));
 				
 				let currentDate = new Date().toLocaleString();
@@ -179,14 +256,8 @@ $(document).ready(function(){
 				$('html, body').animate({
 					scrollTop: $("#resultsTable").offset().top
 				}, 1000);
-				
-				let print_retail_price = '<table id="input_data_table_1"> <tr> <td id="totalCost" colspan="2" class="text-center">Retail Price is : $' + numberWithCommas(clientPrice) +  '</td></tr>';
-				print_retail_price += print_ind_data;
-				
-				print_retail_price +=	'</table>';
-				
-				
-				let print_header = '<table class="img_td table_print_1">' +
+								
+				let print_header = '<table class="img_td table_print_1 table_print_pdf" id="table_print_1">' +
 										'<tr class="img_td">' +
 											'<td><img src="images/logo.png" alt="logo" id="logo_print" class=""/> </td>' +
 											'<td class="text-right"><br/> <span id="date">' + 
@@ -203,20 +274,8 @@ $(document).ready(function(){
 									
 				
 				$('#editor').append(print_header);
-				
-				$('#editor').append(print_retail_price);
-				
-				
-				let print_total_price = '<table id="input_data_table_2"> <tr> <td id="totalCost" colspan="2" class="text-center">Total Cost Price is : $' + 
-											numberWithCommas(totalCost) +  
-											'</td></tr></table>'+
-											'<div class="table_print_1"><br/>' + 
-											'<h2>Input Data</h2>'+
-											'<br/></div>';
-				
-				$('#editor').append(print_total_price);
-				
-				
+				$('#editor').append(print_project_info);
+				$('#editor').append(print_cost_table);								
 				$('#editor').append(print_input_data);
 				
 			});	
